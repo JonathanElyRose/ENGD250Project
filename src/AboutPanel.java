7ed492638158a37808a909823781be2c86a43eb3
Liz Stutz <stutzej@rose-hulman.edu>
2020-04-20 09:10:58 -0500

    updated text on about and help pages using JTextFields

--- src/AboutPanel.java
+++ src/AboutPanel.java
@@ -1,6 +1,6 @@
 import javax.swing.GroupLayout;
 import javax.swing.JLabel;
-
+import javax.swing.JTextField;
 /*
  * @author Emmi Schwitters.
  */
@@ -16,9 +16,20 @@ public class AboutPanel extends ParentPanel {
 	public void setupComponents() {
 		// TODO Auto-generated method stub
 		JLabel aboutTitle = new JLabel("ABOUT");
+		JTextField field = new JTextField();
+		JTextField field1 = new JTextField();
+		JTextField field2 = new JTextField();
+		
+		field.setText("As Engineering Design Students at Rose-Hulman Institute of Technology in Terre Haute, Indiana, we were"
+				+ " challenged to create a computer application designed for use by a culture different from our own.  Our team ");
+		field1.setText("selected Hong Kong as our cultural focus on our animation app, due to its’ incredible diversity.  This product "
+				+ "will allow users to create entertaining animation sequences using a combination of traditional hand-drawn skills");
+		field2.setText(" along with a modern app that allows them to bring their stories to life.");
 		
 		addComponent("aboutTitle", aboutTitle);
-
+		addComponent("field", field);
+		addComponent("field1", field1);
+		addComponent("field2", field2);
 	}
 
 	@Override
@@ -29,15 +40,19 @@ public class AboutPanel extends ParentPanel {
 		
 		getLayout().setHorizontalGroup(
 				getLayout().createParallelGroup(GroupLayout.Alignment.LEADING)
-					.addGroup(getLayout().createSequentialGroup()
-							.addComponent(returnComponent("aboutTitle")))
+					.addGroup(getLayout().createParallelGroup()
+							.addComponent(returnComponent("aboutTitle"))
+							.addComponent(returnComponent("field"))
+							.addComponent(returnComponent("field1"))
+							.addComponent(returnComponent("field2")))
 		);
 		
 		getLayout().setVerticalGroup(
-				getLayout().createParallelGroup(GroupLayout.Alignment.LEADING)
-					.addGroup(getLayout().createSequentialGroup()
-						.addGroup(getLayout().createParallelGroup(GroupLayout.Alignment.CENTER)
-								.addComponent(returnComponent("aboutTitle"))))
+				getLayout().createSequentialGroup()
+								.addComponent(returnComponent("aboutTitle"))
+								.addComponent(returnComponent("field"))
+								.addComponent(returnComponent("field1"))
+								.addComponent(returnComponent("field2"))
 		);
 
 	}
