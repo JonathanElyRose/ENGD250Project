3ddfcf069c0ffced0014d7ab7c4506a372135d2d
Liz Stutz <stutzej@rose-hulman.edu>
2020-04-22 09:51:26 -0500

    added JTextArea instead of JTextField for a cleaner look.

--- src/AboutPanel.java
+++ src/AboutPanel.java
@@ -1,6 +1,7 @@
 import javax.swing.GroupLayout;
 import javax.swing.JLabel;
 import javax.swing.JTextField;
+import javax.swing.JTextArea;
 /*
  * @author Emmi Schwitters, Liz Stutz.
  */
@@ -16,20 +17,17 @@ public class AboutPanel extends ParentPanel {
 	public void setupComponents() {
 		// TODO Auto-generated method stub
 		JLabel aboutTitle = new JLabel("ABOUT");
-		JTextField field = new JTextField();
-		JTextField field1 = new JTextField();
-		JTextField field2 = new JTextField();
+		JTextArea area = new JTextArea();
+
 		
-		field.setText("As Engineering Design Students at Rose-Hulman Institute of Technology in Terre Haute, Indiana, we were"
-				+ " challenged to create a computer application designed for use by a culture different from our own.  Our team ");
-		field1.setText("selected Hong Kong as our cultural focus on our animation app, due to its’ incredible diversity.  This product "
-				+ "will allow users to create entertaining animation sequences using a combination of traditional hand-drawn skills");
-		field2.setText(" along with a modern app that allows them to bring their stories to life.");
-		
+		area.setText("As Engineering Design Students at Rose-Hulman Institute of Technology in Terre Haute, Indiana, we were "
+				+ "challenged to create a computer application \ndesigned for use by a culture different from our own.  Our team "
+				+ "selected Hong Kong as our cultural focus on our \nanimation app, due to its’ incredible diversity.  This product "
+				+ "will allow users to create entertaining animation \nsequences using a combination of traditional hand-drawn skills "
+				+ "along with a modern app that allows them to bring their stories to life.");
+
 		addComponent("aboutTitle", aboutTitle);
-		addComponent("field", field);
-		addComponent("field1", field1);
-		addComponent("field2", field2);
+		addComponent("area", area);
 	}
 
 	@Override
@@ -42,17 +40,13 @@ public class AboutPanel extends ParentPanel {
 				getLayout().createParallelGroup(GroupLayout.Alignment.LEADING)
 					.addGroup(getLayout().createParallelGroup()
 							.addComponent(returnComponent("aboutTitle"))
-							.addComponent(returnComponent("field"))
-							.addComponent(returnComponent("field1"))
-							.addComponent(returnComponent("field2")))
+							.addComponent(returnComponent("area")))
 		);
 		
 		getLayout().setVerticalGroup(
 				getLayout().createSequentialGroup()
 								.addComponent(returnComponent("aboutTitle"))
-								.addComponent(returnComponent("field"))
-								.addComponent(returnComponent("field1"))
-								.addComponent(returnComponent("field2"))
+								.addComponent(returnComponent("area"))
 		);
 
 	}
--- src/HelpPanel.java
+++ src/HelpPanel.java
@@ -3,6 +3,7 @@ import javax.swing.JLabel;
 import javax.swing.JTextField;
 import java.awt.FlowLayout;
 import javax.swing.JFrame;
+import javax.swing.JTextArea;
 /*
  * @author Emmi Schwitters, Liz Stutz.
  */
@@ -27,56 +28,28 @@ public class HelpPanel extends ParentPanel {
 	public void setupComponents() {
 		// TODO Auto-generated method stub
 		JLabel helpTitle = new JLabel("HELP");
-		JTextField field = new JTextField();
-		JTextField field1 = new JTextField();
-		JTextField field2 = new JTextField();
-		JTextField field3 = new JTextField();
-		JTextField field4 = new JTextField();
-		JTextField field5 = new JTextField();
-		JTextField field6 = new JTextField();
-		JTextField field7 = new JTextField();
-		JTextField field8 = new JTextField();
+		
+		JTextArea area = new JTextArea();
+		
+		
+		area.setText("How do I create a new project?\n\n To create a new project, go to the home page and select “New Project”.  A window will then pop up asking you to name your project and import images to be used in the project.\n\n"
+				+ "How do I find my ongoing projects?\n\n"
+				+ "If you go to the menu bar at the top of the application, you will find a page labeled “Projects”.  Click on this and you will be taken to a page that lists all of your projects.  If you want to find a\n project you have been working on recently,\n they can be found at the top of the Home page.\n\n"
+				+ "How do I import photos?\n\n"
+				+ "There are a few different ways to upload images.  One way is to go to the Home page and click on the button on the bottom right labeled “Import Photos”.  This will take you to your computer’s\n files and allow you to select which\n photos you want to upload."
+				+ "Another way to import photos is when you first create a new project.  After asking you to name the project, there is a button that is labeled “Import Images”, which\n will also take you to your computer’s files and allow you to select the images you want to use.\n"
+				+ "A third and final way is to select the “Import Images” button in the project editor.\n\n"
+				+ "How do I save my projects?\n");
+		
+		
+		
 	
 		
-		field.setText("How do I create a new project?");
-		field1.setText("To create a new project, go to the home page and select “New Project”.  A window will then pop up asking you"
-				+ " to name your project and import images to be used in the project.");
-		field2.setText("How do I find my ongoing projects?");
-		field3.setText("If you go to the menu bar at the top of the application, you will find a page labeled “Projects”.  Click on"
-				+ " this and you will be taken to a page that lists all of your projects.  If you want to find a project you have "
-				+ "been working on recently, they can be found at the top of the Home page.");
-		field4.setText("How do I import photos?");
-		field5.setText("There are a few different ways to upload images.  One way is to go to the Home page and click on the button "
-				+ "on the bottom right labeled “Import Photos”.  This will take you to your computer’s files and allow you to "
-				+ "select which photos you want to upload.  ");
-		field6.setText("Another way to import photos is when you first create a new project.  After asking you to name the project,"
-				+ " there is a button that is labeled “Import Images”, which will also take you to your computer’s files and allow"
-				+ " you to select the images you want to use.");
-		field7.setText("A third and final way is to select the “Import Images” button in the project editor.");
-		field8.setText("How do I save my projects?");
-		
-		
-		field.setEditable(false);
-		field1.setEditable(false);
-		field2.setEditable(false);
-		field3.setEditable(false);
-		field4.setEditable(false);
-		field5.setEditable(false);
-		field6.setEditable(false);
-		field7.setEditable(false);
-		field8.setEditable(false);
 	
+		area.setEditable(false);
 		
 		addComponent("helpTitle", helpTitle);
-		addComponent("field", field);
-		addComponent("field1", field1);
-		addComponent("field2", field2);
-		addComponent("field3", field3);
-		addComponent("field4", field4);
-		addComponent("field5", field5);
-		addComponent("field6", field6);
-		addComponent("field7", field7);
-		addComponent("field8", field8);
+		addComponent("area", area);
 	}
 
 	@Override
@@ -89,29 +62,13 @@ public class HelpPanel extends ParentPanel {
 				getLayout().createParallelGroup(GroupLayout.Alignment.LEADING)
 					.addGroup(getLayout().createParallelGroup()
 							.addComponent(returnComponent("helpTitle"))
-							.addComponent(returnComponent("field"))
-							.addComponent(returnComponent("field1"))
-							.addComponent(returnComponent("field2"))
-							.addComponent(returnComponent("field3"))
-							.addComponent(returnComponent("field4"))
-							.addComponent(returnComponent("field5"))
-							.addComponent(returnComponent("field6"))
-							.addComponent(returnComponent("field7"))
-							.addComponent(returnComponent("field8")))
+							.addComponent(returnComponent("area")))
 		);
 		
 		getLayout().setVerticalGroup(
 				getLayout().createSequentialGroup()
-								.addComponent(returnComponent("helpTitle"))
-								.addComponent(returnComponent("field"))
-								.addComponent(returnComponent("field1"))
-								.addComponent(returnComponent("field2"))
-								.addComponent(returnComponent("field3"))
-								.addComponent(returnComponent("field4"))
-								.addComponent(returnComponent("field5"))
-								.addComponent(returnComponent("field6"))
-								.addComponent(returnComponent("field7"))
-								.addComponent(returnComponent("field8"))
+								.addComponent(returnComponent("helpTitle"))		
+								.addComponent(returnComponent("area"))
 		);
 
 	}
