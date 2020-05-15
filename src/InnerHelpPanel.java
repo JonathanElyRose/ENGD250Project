36ceadb4809b9e1ae12bb4fec5ff35e8b816a455
Liz Stutz <stutzej@rose-hulman.edu>
2020-05-15 09:58:11 -0500

    edit help page

--- src/InnerHelpPanel.java
+++ src/InnerHelpPanel.java
@@ -38,19 +38,21 @@ public class InnerHelpPanel extends ParentPanel {
 				+ "Another way to import photos is when you first create a new project.  After asking you to name the project, there is a button that \nis labeled “Import Images”, which will also take you to your computer’s files and allow you to select the images you want to use.  "
 				+ "A third and final way is to select the “Import Images” button \nin the project editor.");
 				area4.setText("How do I save my projects?\n\n"
-				+ "To save a project, go to the projects tab, select the project you want to save, and click the 'Export Animation.'\n\n");
+				+ "To save a project, go to the projects tab, select the project you want to save, and click the 'Export Animation.'");
 
 		ImageIcon icon1 = new ImageIcon("Help part 1.png");
 		ImageIcon icon2 = new ImageIcon("Help part 2.png");
 		ImageIcon icon3 = new ImageIcon("Help part 3.png");
 		ImageIcon icon4 = new ImageIcon("Help part 4.png");
 		ImageIcon icon5 = new ImageIcon("Help part 5.png");
+		ImageIcon icon6 = new ImageIcon("Help part 6.png");
 		
 		java.awt.Image img1 = icon1.getImage();
 		java.awt.Image img2 = icon2.getImage();
 		java.awt.Image img3 = icon3.getImage();
 		java.awt.Image img4 = icon4.getImage();
 		java.awt.Image img5 = icon5.getImage();
+		java.awt.Image img6 = icon6.getImage();
 		
 		int iconSize1 = 1000;
 		int iconSize2 =440;
@@ -59,23 +61,27 @@ public class InnerHelpPanel extends ParentPanel {
 		Image newimg3 = img3.getScaledInstance(iconSize1, iconSize2, java.awt.Image.SCALE_SMOOTH ) ;
 		Image newimg4 = img4.getScaledInstance(iconSize1, iconSize2, java.awt.Image.SCALE_SMOOTH ) ;
 		Image newimg5 = img5.getScaledInstance(iconSize1, iconSize2, java.awt.Image.SCALE_SMOOTH ) ;
+		Image newimg6 = img6.getScaledInstance(iconSize1, iconSize2, java.awt.Image.SCALE_SMOOTH ) ;
 		
 		icon1 = new ImageIcon( newimg1 );
 		icon2 = new ImageIcon( newimg2 );
 		icon3 = new ImageIcon( newimg3 );
 		icon4 = new ImageIcon( newimg4 );
 		icon5 = new ImageIcon( newimg5 );
+		icon6 = new ImageIcon( newimg6 );
 		JLabel label1 = new JLabel(icon1);
 		JLabel label2 = new JLabel(icon2);
 		JLabel label3 = new JLabel(icon3);
 		JLabel label4 = new JLabel(icon4);
 		JLabel label5 = new JLabel(icon5);
+		JLabel label6 = new JLabel(icon6);
 		
 		addComponent("label1", label1);
 		addComponent("label2", label2);
 		addComponent("label3", label3);
 		addComponent("label4", label4);
 		addComponent("label5", label5);
+		addComponent("label6", label6);
 	
 	
 		
@@ -106,7 +112,8 @@ public class InnerHelpPanel extends ParentPanel {
 							.addComponent(returnComponent("area3"))
 							.addComponent(returnComponent("label2"))
 							.addComponent(returnComponent("label5"))
-							.addComponent(returnComponent("area4"))));
+							.addComponent(returnComponent("area4"))
+							.addComponent(returnComponent("label6"))));
 				
 		
 		getLayout().setVerticalGroup(
@@ -122,6 +129,7 @@ public class InnerHelpPanel extends ParentPanel {
 								.addComponent(returnComponent("label2"))
 								.addComponent(returnComponent("label5"))
 								.addComponent(returnComponent("area4"))
+								.addComponent(returnComponent("label6"))
 				
 		);
 
